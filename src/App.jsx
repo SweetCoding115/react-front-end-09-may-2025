f039f120baccfd8626d24ec907df3cb5666e92c2
SweetCoding115 <jessie.pangilinan11111988@gmail.com>
2025-05-09 13:54:00 +0100

    filter by key and callback function for each fileter condition

--- src/App.jsx
+++ src/App.jsx
@@ -42,17 +42,38 @@ function App(props) {
 	}
 
 	const [tasks, setTasks] = useState(props.tasks);
-	const taskList = tasks?.map((task) => (
-		<Todo
-			id={task.id}
-			name={task.name}
-			completed={task.completed}
-			key={task.id}
-			toggleTaskCompleted={toggleTaskCompleted}
-			deleteTask={deleteTask}
-			editTask={editTask}
+	const [filter, setFilter] = useState("All");
+
+	const FILTER_MAP = {
+		All: () => true,
+		Active: (task) => !task.completed,
+		Completed: (task) => task.completed,
+	};
+
+	const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+	const filterList = FILTER_NAMES.map((name) => (
+		<FilterButton
+			key={name}
+			name={name}
+			isPressed={name === filter}
+			setFilter={setFilter}
 		/>
 	));
+
+	const taskList = tasks
+		.filter(FILTER_MAP[filter])
+		.map((task) => (
+			<Todo
+				id={task.id}
+				name={task.name}
+				completed={task.completed}
+				key={task.id}
+				toggleTaskCompleted={toggleTaskCompleted}
+				deleteTask={deleteTask}
+				editTask={editTask}
+			/>
+		));
 
 	const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
 	const headingText = `${taskList.length} ${tasksNoun} remaining`;
@@ -62,9 +83,7 @@ function App(props) {
 			<h1>TodoMatic</h1>
 			<Form addTask={addTask} />
 			<div className="filters btn-group stack-exception">
-				<FilterButton />
-				<FilterButton />
-				<FilterButton />
+				{filterList}
 			</div>
 			<h2 id="list-heading">{ headingText }</h2>
 			<ul
