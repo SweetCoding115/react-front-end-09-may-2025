2f8a45532aa7dd9c6f3c6f540812975913557d86
SweetCoding115 <jessie.pangilinan11111988@gmail.com>
2025-05-11 00:47:16 +0100

    custom hook usePrevious for tracking changes of values

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+function usePrevious(value) {
+	const ref = useRef();
+	useEffect(() => {
+		ref.current = value;
+	});
+	return ref.current;
+}
 
 function Todo(props) {
 	const [isEditing, setEditing] = useState(false);
 	const [newName, setNewName] = useState("");
+	const editFieldRef = useRef(null);
+	const editButtonRef = useRef(null);
+	const wasEditing = usePrevious(isEditing);
 
 	function handleChange(e) {
 		setNewName(e.target.value);
@@ -14,6 +25,15 @@ function Todo(props) {
 		setNewName("");
 		setEditing(false);
 	}
+
+	useEffect(() => {
+		console.log([wasEditing, isEditing])
+		if (!wasEditing && isEditing) {
+			editFieldRef.current.focus();
+		} else if (wasEditing && !isEditing) {
+			editButtonRef.current.focus();
+		}
+	}, [wasEditing, isEditing]);
 
 	const editingTemplate = (
 		<form className="stack-small" onSubmit={handleSubmit}>
@@ -27,6 +47,7 @@ function Todo(props) {
 					type="text"
 					value={newName}
 					onChange={handleChange}
+					ref={editFieldRef}
 				/>
 			</div>
 			<div className="btn-group">
@@ -64,7 +85,8 @@ function Todo(props) {
 					className="btn"
 					onClick={() => {
 						setEditing(true);
-					}}>
+					}}
+					ref={editButtonRef}>
 					Edit <span className="visually-hidden">{props.name}</span>
 				</button>
 				<button
